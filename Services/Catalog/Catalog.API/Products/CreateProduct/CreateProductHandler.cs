596ae7843b8d2fb6e34b3fb55b5f99eed3f39287
Kasra Nadri <kasra.n2018@gmail.com>
2025-08-12 11:59:56 +0430

    installed marten and created a post endpoint

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,13 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
-using MediatR;
+using Marten;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string>Category, string Description, 
         string ImageFile, decimal Price ) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async  Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -22,7 +22,10 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
 
         }
     }
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //===== ADD SERVICES TO THE CONTAINER =====\\
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
 //===== CONFIGURE THE HTTP REQUEST PIPELINE =====\\
+app.MapCarter();
 
 app.Run();
